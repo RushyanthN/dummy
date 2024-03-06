448b054e0c09c717b39693419caa3d2b4ff8c2d8
RushyanthN <156248625+RushyanthN@users.noreply.github.com>
2024-03-07 02:59:44 +0530

    making first commit

---
+++ prompt.py
@@ -0,0 +1,15 @@
+def open_file(corpus):
+    with open(corpus, 'r') as file:
+        contents = file.read_lines()
+    return contents
+
+
+
+def report_count(token):
+    text = open_file('coupus.txt')
+    count=0
+    for line in text:
+        for word in line.split():
+            if word == token
+            count+=1
+    return count
